36c1ac3e77e51c3bdbcc4904ae0634d1d9f7a204
Hammas ullah Naik <hammasullah.naik@gmail.com>
2021-08-21 16:43:06 +0500

    Service Added

---
+++ WeatherApp(Combine)/Service/Service.swift
@@ -0,0 +1,31 @@
+//
+//  Service.swift
+//  WeatherApp(Combine)
+//
+//  Created by Hammas Naik on 20/08/2021.
+//
+
+import Foundation
+import Combine
+
+class WebService
+{
+    var util = Utils()
+    func getWeather() -> AnyPublisher<Weather?,Error>
+    {
+        guard let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Housten&appid=7d2dd8c9c5578b741c7735ad3f0d39ea&units=imperial")
+        else
+        {
+            fatalError("Api Error")
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+                .map(\.data)
+                .print()
+                .decode(type: WeatherResponse.self, decoder: JSONDecoder())
+                .print()
+                .map(\.main)
+                .receive(on: RunLoop.main)
+                .eraseToAnyPublisher()
+    }
+}
--- WeatherApp(Combine)/ViewController.swift
+++ WeatherApp(Combine)/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController
 {
+//MARK: - OUTLETS
+    @IBOutlet weak var weather: UILabel!
 
+//MARK: - VARIABLES
+    var weatherapi = WebService()
+    var cancellable :AnyCancellable?
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+         cancellable = weatherapi.getWeather()
+            .catch{ _ in Just(Weather.placeholder) }
+            .map
+            {data in
+                if let temp = data?.temp
+                {
+                    return "\(temp) ℉"
+                }
+                else
+                {
+                    return "Empty"
+                }
+            }
+            .assign(to: \.text, on: self.weather)
+            
     }
+    
 
 
 }
